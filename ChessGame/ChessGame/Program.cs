3021b3cc8e7b77988f3ff104318b980f964df9a8
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-24 17:42:51 +0000

    Improving the implementation of the PrintBoard method

--- ChessGame/ChessGame/Program.cs
+++ ChessGame/ChessGame/Program.cs
@@ -8,32 +8,24 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-
-            ChessPosition position = new ChessPosition('c', 7);
-
-            Console.WriteLine(position);
-
-            Console.WriteLine(position.ChessPositionToMatrixPosition());
-
-            
-            //try
-            //{
-            //    Board board = new Board(8, 8);
+            try
+            {
+                Board board = new Board(8, 8);
 
 
-            //    board.PlacePieceOnBoard(new Rook(Color.Black, board), new PositionBoard(0, 0));
-            //    board.PlacePieceOnBoard(new Rook(Color.Black, board), new PositionBoard(1, 3));
-            //    board.PlacePieceOnBoard(new King(Color.Black, board), new PositionBoard(0, 2));
+                board.PlacePieceOnBoard(new Rook(Color.Black, board), new PositionBoard(0, 0));
+                board.PlacePieceOnBoard(new Rook(Color.Black, board), new PositionBoard(1, 3));
+                board.PlacePieceOnBoard(new King(Color.Black, board), new PositionBoard(0, 2));
 
+                board.PlacePieceOnBoard(new Rook(Color.White, board), new PositionBoard(3, 5));
+                Screen.PrintBoard(board);
 
-            //    Screen.PrintBoard(board);
-
-            //    Console.ReadLine();
-            //}
-            //catch (BoardException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+                Console.ReadLine();
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- ChessGame/ChessGame/Screen.cs
+++ ChessGame/ChessGame/Screen.cs
@@ -11,6 +11,9 @@ namespace ChessGame
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                //It will print on the screen the numbers corresponding to each line (1-8)
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < board.Columms; j++)
                 {
                     // If there is no piece in row i column j
@@ -21,10 +24,30 @@ namespace ChessGame
                     else
                     // If it exists then place the piece
                     {
-                        Console.Write(board.Piece(i,j) + " ");
+                        PrintPieceOfDifferentColor(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h ");
+        }
+
+        // It will change the color of the piece, in case the piece is black
+        public static void PrintPieceOfDifferentColor(Piece piece)
+        {
+            // If the piece is white, print the piece with the normal color of the console.
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                // If the piece is black prints the piece in blue
+                ConsoleColor newPieceColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write(piece);
+                Console.ForegroundColor = newPieceColor;
             }
         }
     }
